ca1b128a95094639fd179532991bc308e2707d3d
popcornmotte <popcornmotte@gmx.net>
2020-12-12 12:37:49 +0100

    heyo

--- Scripts/window.gd
+++ Scripts/window.gd
@@ -6,11 +6,22 @@ extends Node2D
 # var b = "text"
 var dragging = false
 var pos : Vector2
+var content
+var CONTENT 
+export var windowName : String
+export var test = false
+export var testScene : String
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$OuterFrame/ProgramName.text = windowName
+	if test:
+		loadContent(testScene)
 	pass # Replace with function body.
 
-
+func loadContent(c : String):
+	var CONTENT = load(c)
+	content = CONTENT.instance()
+	$OuterFrame/InnerFrame/WindowContent.add_child(content)
 
 func _process(delta):
 	if dragging:
