4f9662a526cd97c1af70639e4d633a63503ade31
Tectato <lukasmoersler@gmail.com>
2020-12-12 16:39:20 +0100

    did minimal stuff

--- Scripts/InvadersInvader.gd
+++ Scripts/InvadersInvader.gd
@@ -1,7 +1,19 @@
 extends Area2D
 
+var downSpeed = 10
+var sideSpeed = 20
+var dir = 1
 
-func _on_Invader_area_entered(area):
+func _process(delta):
+	translate(Vector2(sideSpeed * delta, downSpeed * delta))
+	pass
+
+func _on_Bullet_area_entered(area):
 	area.queue_free()
 	queue_free()
-	pass # Replace with function body.
+	pass
+
+func _on_InvaderSpace_area_exited(area):
+	dir *= -1
+	translate(Vector2(0, sideSpeed))
+	pass 
