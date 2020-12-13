c0eff030d0b140e5e834d2649e0fba462e8bd260
Tectato <lukasmoersler@gmail.com>
2020-12-13 04:09:15 +0100

    added ram shake

--- Scripts/RAM.gd
+++ Scripts/RAM.gd
@@ -1,20 +1,36 @@
 extends Control
 
+var labelOrigin
+var offset = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	labelOrigin = $MarginContainer/Polygon2D.position.x
+	$MarginContainer/Polygon2D/Warning.visible = false
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$MarginContainer/ProgressBar.value = global.ram
 	$MarginContainer/Polygon2D/Label.text = "Available RAM"\
 	+" "+str(global.MAXRAM-global.ram)+"/"+str(global.MAXRAM)+" MB"
 	pass
 
+func notEnoughRam(missingRam):
+	offset = 6
+	$MarginContainer/Polygon2D/Warning.text = str(missingRam) + " MB more RAM required"
+	$MarginContainer/Polygon2D/Warning.visible = true
+	pass
+
+func _on_Timer_timeout():
+	if(abs(offset) > 0.1):
+		$MarginContainer/Polygon2D.position.x = labelOrigin + offset
+		offset -= 1.9 * offset
+	else:
+		$MarginContainer/Polygon2D.position.x = labelOrigin
+		$MarginContainer/Polygon2D/Warning.visible = false
+	pass
+
+
+func _on_Button_button_down():
+	notEnoughRam(4)
+	pass # Replace with function body.
